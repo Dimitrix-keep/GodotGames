83505f26937a29c565cf6b6af49f3493bedf3a2f
Dimitrix-keep <Dimaapariciopereira2@gmail.com>
2026-01-16 14:45:16 +0100

    Subiendo Datos nuevos de estados

--- fisher-claim-master/Scripts/player.gd
+++ fisher-claim-master/Scripts/player.gd
@@ -47,10 +47,10 @@ func _anim_name(state: PlayerState) -> String:
 
 
 func change_state(_new_state: PlayerState):
-	if current_state == _new_state:
-		return true
-	if _new_state:
-		current_state = _new_state
+	if current_state==_new_state:
+		return
+	if can_auto_translate():
+		current_state=_new_state
 		var _name = _anim_name(current_state)
 		state_machine.travel(_name)
 		print("Estado actual:", _name)
@@ -79,9 +79,6 @@ func _physics_process(delta):
 		velocity.x = direction * WALK_SPEED
 		change_state(PlayerState.WALK)
 		flip_sprite(direction)
-	elif Input.is_action_pressed("fishing"):
-		change_state(PlayerState.FISHING)
-		
 	elif Input.is_action_pressed("reel") : #and pez=null:
 		change_state(PlayerState.REEL)
 	else:
@@ -92,5 +89,10 @@ func _physics_process(delta):
 
 # Cambia "anim_name" por "_anim_name"
 func _on_animation_tree_animation_finished(anim_name: StringName):
-	print("Termino la animacion de ", state_machine.get_current_node())
+	print("Termino el estado animado de ", state_machine.get_current_node())
 	# Ya no saldrá el aviso de "UNUSED_PARAMETER"
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print("Que pasa aqui")
+	pass # Replace with function body.
